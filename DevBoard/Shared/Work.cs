79d8ea0ef2a2d1a1da6b904da1afc02934507fa2
Валентин Долгов <wh-rabbit@mail.ru>
2021-12-26 20:41:17 +0500

    Приблизительный вид того что должно быть

--- DevBoard/Shared/Work.cs
+++ DevBoard/Shared/Work.cs
@@ -8,26 +8,15 @@ namespace DevBoard.Shared
 {
     public class Work
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string WhatsUp { get; set; }
         public DateTime Start { get; set; }
         public DateTime Stop => Start.AddMinutes(Duration);
         public int Duration { get; set; }
+        public int ParentWorkId { get; set; }
+        public Work ParentWork { get; set; }
 
         #region Строковое представление времени.
-        public string GetStartTime()
-        {
-            string hour = GetTimeWithZero(Start.Hour);
-            string minute = GetTimeWithZero(Start.Minute);
-            return $"{hour} : {minute}";
-        }
-        public string GetStopTime()
-        {
-            string hour = GetTimeWithZero(Stop.Hour);
-            string minute = GetTimeWithZero(Stop.Minute);
-            return $"{hour} : {minute}";
-        }
         public string GetDuration()
         {
             var hour = GetTimeWithZero(Duration / 60);
@@ -69,92 +58,179 @@ namespace DevBoard.Shared
         {
             var works = new List<Work>()
             {
-                new Work(){
-                    Start = new DateTime(2021, 12, 11, 17, 5, 0), 
-                    Duration = 17, 
-                    Name = "DevBoard", 
-                    Description = "Подготовка;", 
-                    WhatsUp = "Старт проекта; Макет основной таблицы с заданиями;"},
-                new Work(){
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 11, 17, 5, 0),
+                    Duration = 1,
+                    Name = "DevBoard"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 11, 17, 5, 0),
+                    Duration = 7,
+                    Name = "Создать шаблон проекта."
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 11, 17, 13, 0),
+                    Duration = 10,
+                    Name = "Создать макет основной таблицы с заданиями."
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 11, 20, 5, 0), 
-                    Duration = 65, 
-                    Name = "DevBoard", 
-                    Description = "Ошибки при сборке проекта;", 
-                    WhatsUp = "Попытка вынести в кодбихайнд; Переименование основного файла показа работ; Вынес ссылку на доску в меню;" },
-                new Work(){
+                    Duration = 55, 
+                    Name = "Вынести в кодбихайнд."
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 11, 20, 5, 0),
+                    Duration = 10,
+                    Name = "Вынести ссылку на доску в меню.",
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 11, 23, 20, 0), 
                     Duration = 34, 
-                    Name = "DevBoard", 
-                    Description = "Модель данных", 
-                    WhatsUp = "Основная модель работы;" },
-                new Work(){Start = new DateTime(2021, 12, 12, 11, 47, 0), 
+                    Name = "Разработать модель данных"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 11, 47, 0), 
                     Duration = 15, 
-                    Name = "DevBoard", 
-                    Description = "Добавить колонку для заданий;", 
-                    WhatsUp = "Добавление колонки задание; " },
-                new Work(){
+                    Name = "Добавить колонку для заданий"
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 12, 12, 11, 0), 
-                    Duration = 24, 
-                    Name = "DevBoard", 
-                    Description = "Поправить часы в разметке; Изменить модель отображение времени (с доминирующими нулями)", 
-                    WhatsUp = "Убрал перенос текста в колонках с временем;" },
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 12, 43, 0), 
-                    Duration = 12, 
-                    Name = "DevBoard", 
-                    Description = "Разобраться с доминирующими нулями во времени;", 
-                    WhatsUp = "Активировал метод преобразования DateTime в строковое представление с доминирующими нулями;" },
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 13, 22, 0), 
+                    Duration = 10, 
+                    Name = "Поправить часы в разметке."
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 12, 22, 0),
+                    Duration = 26,
+                    Name = "Изменить модель отображение времени (с доминирующими нулями)"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 00, 0), 
                     Duration = 16, 
-                    Name = "DevBoard", 
-                    Description = "Исправить меню для стартовой board;", 
-                    WhatsUp = "Настроил перевод со стартовой страницы на board;" },
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 13, 55, 0), 
-                    Duration = 23, 
-                    Name = "DevBoard", 
-                    Description = "Финишное время;", 
-                    WhatsUp = "Добавил в Work метод для возвращения финишного времени коллекции; Разметка таблицы board для фиишного времени; Добавил доминирующие нули в финишное время;" },
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 14, 28, 0), 
-                    Duration = 17, 
-                    Name = "DevBoard", 
-                    Description = "Лэйбл для финишного времени board;", 
-                    WhatsUp = "Добавил лэйбл в фстолбец с описание того что сделал; Центрировал содержимое по правому краю;" },
-                new Work(){
+                    Name = "Исправить меню для стартовой board;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 18, 0), 
+                    Duration = 5, 
+                    Name = "Добавил в Work метод для возвращения финишного времени коллекции;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 28, 0),
+                    Duration = 11,
+                    Name = "Разметка таблицы board для фиишного времени."
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 39, 0),
+                    Duration = 7,
+                    Name = "Добавить доминирующие нули в финишное время"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 47, 0), 
+                    Duration = 7, 
+                    Name = "Добавить лэйбл в фстолбец с описание того что сделал"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 14, 55, 0),
+                    Duration = 10,
+                    Name = "Разметка финишного времени"
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 12, 15, 11, 0), 
                     Duration = 11, 
-                    Name = "DevBoard", 
-                    Description = @"Значок плэй в кнопке действия (подсмотреть значки в менюшке);", 
-                    WhatsUp = "добавил значок play в каждую строку; " },
-                new Work(){
+                    Name = "Добавить значок play в каждую строку"
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 12, 16, 6, 0),
-                    Duration = 24,
-                    Name = "DevBoard",
-                    Description = @"Git;",
-                    WhatsUp = "Изучение работы с Git в VS; Авторизация и подключение к репозиторию; Проверка сохранения изменний;" },                
-                new Work(){
+                    Duration = 12,
+                    Name = "Изучить основы работы с Git в VS."
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 16, 18, 0),
+                    Duration = 11,
+                    Name = "Авторизация и подключение к репозиторию;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 16, 29, 0),
+                    Duration = 17,
+                    Name = "Проверка сохранения изменний."
+                },
+                new Work()
+                {
                     Start = new DateTime(2021, 12, 12, 17, 1, 0),
                     Duration = 15,
-                    Name = "DevBoard",
-                    Description = @"Проверка измений git в эксплорере vs;",
-                    WhatsUp = "Проверил работоспособность эксплорера git (хуета);" },                
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 17, 10, 0),
+                    Name = "Проверка измений git в эксплорере vs;"
+                },                
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 12, 17, 17, 0),
                     Duration = 22,
-                    Name = "DevBoard",
-                    Description = @"Поменять местами последнюю строку и Всего; Компонент для добавления новой работы;",
-                    WhatsUp = "Добавил формочку для добавления новой работы;" },
-                new Work(){
-                    Start = new DateTime(2021, 12, 12, 0, 0, 0),
+                    Name = "Компонент для добавления новой работы;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 26, 12, 44, 0),
+                    Duration = 73,
+                    Name = "Сделать отдельный компонент для добавления работ;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 26, 16, 0, 0),
+                    Duration = 177,
+                    Name = "Оставить в модели только название работы;"
+                },
+                new Work()
+                {
+                    Start = new DateTime(2021, 12, 26, 20, 1, 0),
+                    Duration = 37,
+                    Name = "Форматирование списка работ."
+                },
+
+                ////////////////Текущая//////////////////////////////////////////
+
+                /////////////////////////////////////////////////////////////////
+                
+                new Work()
+                {
+                    Start = new DateTime(1, 1, 1, 0, 0, 0),
                     Duration = 0,
-                    Name = "DevBoard",
-                    Description = @"
-Доработка формы добавления работ;
-Сделать enum на все статичные ссылки; 
-Сделать принудительный перенос после ';' ;",
-                    WhatsUp = "_________" },
+                    Name = "Распределить ID родительских работ."
+                },
+                new Work()
+                {
+                    Start = new DateTime(1, 1, 1, 0, 0, 0),
+                    Duration = 0,
+                    Name = "Сделать отдельный компонент для добавления работ."
+                },
+                new Work()
+                {
+                    Start = new DateTime(1, 1, 1, 0, 0, 0),
+                    Duration = 0,
+                    Name = "Доработка формы добавления работ."
+                },
+                new Work()
+                {
+                    Start = new DateTime(1, 1, 1, 0, 0, 0),
+                    Duration = 0,
+                    Name = "Сделать enum на все статичные ссылки."
+                },
             };
             return works;
         }
